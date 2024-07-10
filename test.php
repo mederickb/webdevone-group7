3faa98a7c0de9d69c5d6bd15136276a6a7c0d3cd
turtle <159800222+mederickb@users.noreply.github.com>
2024-07-10 13:28:55 -0400

    test file and update gitignore

---
+++ test.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Placeholder Test File</title>
+	</head>
+	<body>
+		<h1>Hello world!</h1>
+
+		<?php
+			echo phpinfo();
+        ?>
+	</body>
+</html>
